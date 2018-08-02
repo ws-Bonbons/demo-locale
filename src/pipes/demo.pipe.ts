132f91e5067bd5f351fa42249c470220bb0328b4
Big Mogician <sunaoran@youzan.com>
2018-08-02 16:34:26 +0800

    update demo

--- src/pipes/demo.pipe.ts
+++ src/pipes/demo.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeMiddleware, PipeOnInit, Logger, PipeFactory, InjectService } from "@bonbons/core";
 import { TestService } from "../service/test";
-import { UUID } from "@bonbons/utils";
+import { UUID, setColor } from "@bonbons/utils";
 import { ABC } from "../service/imp";
 
 @Pipe()
@@ -11,14 +11,14 @@ export class DemoPipe extends PipeMiddleware {
   constructor(private test: TestService, private abc: ABC, private logger: Logger, private injector: InjectService) {
     super();
     console.log("======>create demo pipeline");
-    console.log(injector.get(ABC));
+    // console.log(injector.get(ABC));
   }
 
   async process() {
-    console.log("start demo pipeline");
-    this.logger.debug(`process in pipe [ DemoPipe : ${this.id} ]`);
-    this.logger.debug(`check singleton test service id : [${this.test.id}]`);
-    this.logger.debug(`check scoped abc service id : [${this.abc.show()}]`);
+    console.log("enter demo pipeline");
+    this.logger.debug(`process in pipe [ DemoPipe : ${setColor("green", this.id)} ]`);
+    this.logger.debug(`check singleton test service id : [${setColor("cyan", this.test.id)}]`);
+    this.logger.debug(`check scoped abc service id : [${setColor("red", this.abc.show())}]`);
     console.log("======>leave demo pipeline");
   }
 
